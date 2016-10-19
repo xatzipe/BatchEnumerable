c5f38218845aba0524c006be103ef238e720edfd
Theodore Chatziperis <xatzipe@gmail.com>
2016-10-20 00:38:47 +0300

    Added MultipleBatchEnumerator interface, implementation and tests

---
+++ Xatzipe.BatchEnumerable/Xatzipe.BatchEnumerable/Multiple/AbstractMultipleBatchEnumerator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xatzipe.BatchEnumerable.Multiple
+{
+
+    /// <summary>
+    /// AbstractMultipleBatchEnumerator class
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    public abstract class AbstractMultipleBatchEnumerator<TModel, TResult> : AbstractBatchEnumerator<TModel, TResult>
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected IQueryable<TModel>[] ItemList;
+
+        /// <summary>
+        /// tmp storage durring CurrentBatch creation
+        /// </summary>
+        protected IEnumerable<TResult> PreCurrentBatch = System.Linq.Enumerable.Empty<TResult>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected int CurrentListItem;
+
+        /// <summary>
+        /// Stores the batch number for the current batch list enumeration
+        /// </summary>
+        protected int CurrentBatchNumberLocal;
+
+        /// <summary>
+        /// AbstractMultipleBatchEnumerator contructor
+        /// </summary>
+        /// <param name="itemList"></param>
+        /// <param name="response"></param>
+        /// <param name="order"></param>
+        /// <param name="filter"></param>
+        /// <param name="batchSize"></param>
+        public AbstractMultipleBatchEnumerator (
+            IEnumerable<IQueryable<TModel>> itemList,
+            Expression<Func<TModel, TResult>> response,
+            Func<IQueryable<TModel>, IOrderedQueryable<TModel>> order = null,
+            Expression<Func<TModel, bool>> filter = null,
+            int batchSize = 10
+        ) : base(
+            response,
+            order,
+            filter,
+            batchSize
+        )
+        {
+            if (null == itemList) {
+                throw new ArgumentNullException(nameof(itemList), "Items list cannot be empty");
+            }
+
+            if (0 == itemList.Count()) {
+                throw new ArgumentNullException(nameof(itemList), "Items list cannot be empty");
+            }
+
+            ItemList = itemList.ToArray();
+        }
+
+        /// <summary>
+        ///  returns true if the current enumerator is the last one in the array
+        /// </summary>
+        protected bool AllItemsPassed {
+            get { return ItemList.Count() == (CurrentListItem + 1); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected IQueryable<TModel> CurrentBatchItems {
+            get { return ItemList[CurrentListItem]; }
+        }
+    }
+}
---
+++ Xatzipe.BatchEnumerable/Xatzipe.BatchEnumerable/Multiple/IMultipleBatchEnumerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xatzipe.BatchEnumerable.Multiple
+{
+    /// <summary>
+    /// Enumerator Interface for iterating a List of enumerables in batches
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    public interface IMultipleBatchEnumerator<TModel> : IBatchEnumerator<TModel>
+    {
+    }
+
+    /// <summary>
+    /// Enumerator Interface for iterating a list of enumerable in batches
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    public interface IMultipleBatchEnumerator<TModel, TResult> : IBatchEnumerator<TModel, TResult>
+    {
+    }
+}
