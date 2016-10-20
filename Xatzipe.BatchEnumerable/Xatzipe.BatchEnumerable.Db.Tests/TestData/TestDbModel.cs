e802a19d36f17bbd96f436181930678076a048ab
Theodore Chatziperis <xatzipe@gmail.com>
2016-10-20 16:54:09 +0300

    added new project that introduces batch enumerator extension for DbContext. added the corresponding tests project

---
+++ Xatzipe.BatchEnumerable/Xatzipe.BatchEnumerable.Db.Tests/TestData/TestDbModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xatzipe.BatchEnumerable.Db.Tests.TestData
+{
+    public class TestDbModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
