1dc4841399dd02bef604c8aae08895dad9307974
Theodore Chatziperis <xatzipe@gmail.com>
2016-10-20 22:28:03 +0300

    added BatchEnumeratorAggregate

---
+++ Xatzipe.BatchEnumerable/Xatzipe.BatchEnumerable/Aggregate/IBatchEnumerableAggregate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xatzipe.BatchEnumerable.Aggregate
+{
+    /// <summary>
+    /// Iterates a List of IBatchEnumerables{TModel}
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    public interface IBatchEnumerableAggregate<TModel> : IBatchEnumerable<TModel>
+    {
+    }
+
+}
---
+++ Xatzipe.BatchEnumerable/Xatzipe.BatchEnumerable/Aggregate/IBatchEnumeratorAggregate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xatzipe.BatchEnumerable.Aggregate
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    public interface IBatchEnumeratorAggregate<TModel> : IBatchEnumerator<TModel>
+    {
+    }
+}
